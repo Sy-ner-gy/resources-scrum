fa606eb32e0e18958fba9cab5240199744bc3ea1
Sandra Martini <sandrinhaamartini@gmail.com>
2021-11-13 17:51:17 -0300

    refactor(): normalização do fluxo de trabalho
    * Ao refatorar eu antecipei mudanças manuais que já foram efetuadas ao longo do projeto, esse commit serve para suplimir o excesso de mudanças;

--- README.md
+++ README.md
@@ -3,4 +3,63 @@
 Aplicação para o sorteio das funções dos Scrum para a equipe Sy\ "ner:gy.
 
 ### Site para o sorteio:
-> <http://synergy2.epizy.com/scrum/>
+> <http://synergy2.epizy.com/scrum/>  
+
+
+
+
+
+# Commits
+
+## Sintaxe
+
+```
+ "tipo"("Componente"): "ID" -descrição
+```
+
+* Os valores possíveis para "tipo" são:
+
+  * **build:** usado quando é criado novo(s) arquivo(s) ou pasta(s);
+
+  * **fix:** usado quando o commit é uma solução de um problema no código;
+
+  * **feat:** usado quando se adiciona um novo recurso;
+
+  * **test:** usado quando se faz um teste de novo recurso (test é uma variante de feat);
+
+  * **docs:** usado na criação ou alteração de documentação;
+
+  * **style:** usado quando se modifica o estilo do código ou se faz alguma correção ortográfica (mudança no código que não adiciona, modifica ou remove recurso);
+
+  * **refactor:** usado quando se refatora o código;
+
+
+* **"Componente":** Aqui se especifica o componente ou a estrutura trabalhada (seja o mais específico possível, evite macro-estruturas)
+
+* *"ID":** identificação da tarefa(presente no Trello)
+
+* **"-descrição":** livre para o dev (escrita em inglês)
+
+
+
+
+## Exemplos:
+
+* build(controller): VG01 -add Controller.js file (nesse caso não há como ser mais específico que isso, mas evite usar macroestruturas como o Controller)
+
+* docs(journal): -added file for the day 06/02
+
+
+* feat(page): VW01 -built layout mobile in portrait mode
+
+
+
+* feat(gamerules): -built the Sony
+
+## Obs.:
+
+* Cada commit deverá ser de um tipo e referente a um componente: Caso você esteja trabalhando com mais de um componente, separe as mudanças de cada componente em um commit específico.
+
+* Quando o commit não se refere a nenhuma tarefa no Trello, crie a tarefa(não necessita adicionar detalhes) numa coluna adequada e dê ela uma ID para ser usada no commit e no código.
+
+* Essa é uma convenção da Sy "ner:gy e estará em constante desenvolvimento
--- script.js
+++ script.js
@@ -2,69 +2,69 @@ let names = ["Jean", "Sandra", "Cosmo", "Anna", "Wister", "Luiz"]
 
 
 function sorteio() {
-	//Nome 1
-	let randomName1 = names[Math.floor(Math.random() * names.length)];
+    //Nome 1
+    let randomName1 = names[Math.floor(Math.random() * names.length)];
 
-	let indexName1 = names.indexOf(randomName1);
+    let indexName1 = names.indexOf(randomName1);
 
-	let name1 = names.splice(indexName1, 1)
-
-
-	//Nome 2
-	let randomName2 = names[Math.floor(Math.random() * names.length)];
-
-	let indexName2 = names.indexOf(randomName2);
-
-	let name2 = names.splice(indexName2, 1)
+    let name1 = names.splice(indexName1, 1)
 
 
-	//Nome 3
-	let randomName3 = names[Math.floor(Math.random() * names.length)];
+    //Nome 2
+    let randomName2 = names[Math.floor(Math.random() * names.length)];
 
-	let indexName3 = names.indexOf(randomName3);
+    let indexName2 = names.indexOf(randomName2);
 
-	let name3 = names.splice(indexName3, 1)
-
-
-	//Nome 4
-	let randomName4 = names[Math.floor(Math.random() * names.length)];
-
-	let indexName4 = names.indexOf(randomName4);
-
-	let name4 = names.splice(indexName4, 1)
+    let name2 = names.splice(indexName2, 1)
 
 
-	//Nome 5
-	let randomName5 = names[Math.floor(Math.random() * names.length)];
+    //Nome 3
+    let randomName3 = names[Math.floor(Math.random() * names.length)];
 
-	let indexName5 = names.indexOf(randomName5);
+    let indexName3 = names.indexOf(randomName3);
 
-	let name5 = names.splice(indexName5, 1)
+    let name3 = names.splice(indexName3, 1)
 
 
-	//Nome 6
-	let randomName6 = names[Math.floor(Math.random() * names.length)];
+    //Nome 4
+    let randomName4 = names[Math.floor(Math.random() * names.length)];
 
-	let indexName6 = names.indexOf(randomName6);
+    let indexName4 = names.indexOf(randomName4);
 
-	let name6 = names.splice(indexName6, 1)
+    let name4 = names.splice(indexName4, 1)
 
-	//Conversão em string
 
-	let coluna1 = name1.toString();
-	let coluna2 = name2.toString();
-	let coluna3 = name3.toString();
-	let coluna4 = name4.toString();
-	let coluna5 = name5.toString();
-	let coluna6 = name6.toString();
+    //Nome 5
+    let randomName5 = names[Math.floor(Math.random() * names.length)];
 
-	//Escrita no HTML
-	document.querySelector("#name1").innerHTML = coluna1;
-	document.querySelector("#name2").innerHTML = coluna2;
-	document.querySelector("#name3").innerHTML = coluna3;
-	document.querySelector("#name4").innerHTML = coluna4;
-	document.querySelector("#name5").innerHTML = coluna5;
-	document.querySelector("#name6").innerHTML = coluna6;
+    let indexName5 = names.indexOf(randomName5);
+
+    let name5 = names.splice(indexName5, 1)
+
+
+    //Nome 6
+    let randomName6 = names[Math.floor(Math.random() * names.length)];
+
+    let indexName6 = names.indexOf(randomName6);
+
+    let name6 = names.splice(indexName6, 1)
+
+    //Conversão em string
+
+    let coluna1 = name1.toString();
+    let coluna2 = name2.toString();
+    let coluna3 = name3.toString();
+    let coluna4 = name4.toString();
+    let coluna5 = name5.toString();
+    let coluna6 = name6.toString();
+
+    //Escrita no HTML
+    document.querySelector("#name1").innerHTML = coluna1;
+    document.querySelector("#name2").innerHTML = coluna2;
+    document.querySelector("#name3").innerHTML = coluna3;
+    document.querySelector("#name4").innerHTML = coluna4;
+    document.querySelector("#name5").innerHTML = coluna5;
+    document.querySelector("#name6").innerHTML = coluna6;
 }
 
 
